1963a31e446151ecb74ce4f974c185972195a548
aaronps <aaronperezsanchez@hotmail.com>
2016-12-02 12:18:57 +0800

    Updated netbeans project

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -21,7 +21,7 @@ FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=MinGW-Windows
+CND_PLATFORM=None-Windows
 CND_DLIB_EXT=dll
 CND_CONF=Debug
 CND_DISTDIR=dist
@@ -35,17 +35,17 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/1869484693/SDL_png.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_bmp2png.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_comparetiles.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_converter.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_deletetiles.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_extract.o \
-	${OBJECTDIR}/_ext/1869484693/TSTool_info.o \
-	${OBJECTDIR}/_ext/1869484693/Tileset.o \
-	${OBJECTDIR}/_ext/1869484693/ToolManager.o \
-	${OBJECTDIR}/_ext/1869484693/jsoncpp.o \
-	${OBJECTDIR}/_ext/1869484693/tstool.o \
+	${OBJECTDIR}/_ext/6f6e1295/SDL_png.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_bmp2png.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_comparetiles.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_converter.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_deletetiles.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_extract.o \
+	${OBJECTDIR}/_ext/6f6e1295/TSTool_info.o \
+	${OBJECTDIR}/_ext/6f6e1295/Tileset.o \
+	${OBJECTDIR}/_ext/6f6e1295/ToolManager.o \
+	${OBJECTDIR}/_ext/6f6e1295/jsoncpp.o \
+	${OBJECTDIR}/_ext/6f6e1295/tstool.o \
 	${OBJECTDIR}/src/TSTool_append.o \
 	${OBJECTDIR}/src/TSTool_mapconverter.o \
 	${OBJECTDIR}/src/TSTool_newtileset.o
@@ -74,75 +74,75 @@ LDLIBSOPTIONS=
 tstool-debug.exe: ${OBJECTFILES}
 	${LINK.cc} -o tstool-debug ${OBJECTFILES} ${LDLIBSOPTIONS} -static-libgcc -static-libstdc++ -lpng -lz -lSDL -lmingw32 -mconsole
 
-${OBJECTDIR}/_ext/1869484693/SDL_png.o: /C/prog/c++/tstool/src/SDL_png.c 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.c) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/SDL_png.o /C/prog/c++/tstool/src/SDL_png.c
+${OBJECTDIR}/_ext/6f6e1295/SDL_png.o: /C/prog/c++/tstool/src/SDL_png.c
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.c) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/SDL_png.o /C/prog/c++/tstool/src/SDL_png.c
 
-${OBJECTDIR}/_ext/1869484693/TSTool_bmp2png.o: /C/prog/c++/tstool/src/TSTool_bmp2png.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_bmp2png.o /C/prog/c++/tstool/src/TSTool_bmp2png.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_bmp2png.o: /C/prog/c++/tstool/src/TSTool_bmp2png.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_bmp2png.o /C/prog/c++/tstool/src/TSTool_bmp2png.cpp
 
-${OBJECTDIR}/_ext/1869484693/TSTool_comparetiles.o: /C/prog/c++/tstool/src/TSTool_comparetiles.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_comparetiles.o /C/prog/c++/tstool/src/TSTool_comparetiles.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_comparetiles.o: /C/prog/c++/tstool/src/TSTool_comparetiles.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_comparetiles.o /C/prog/c++/tstool/src/TSTool_comparetiles.cpp
 
-${OBJECTDIR}/_ext/1869484693/TSTool_converter.o: /C/prog/c++/tstool/src/TSTool_converter.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_converter.o /C/prog/c++/tstool/src/TSTool_converter.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_converter.o: /C/prog/c++/tstool/src/TSTool_converter.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_converter.o /C/prog/c++/tstool/src/TSTool_converter.cpp
 
-${OBJECTDIR}/_ext/1869484693/TSTool_deletetiles.o: /C/prog/c++/tstool/src/TSTool_deletetiles.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_deletetiles.o /C/prog/c++/tstool/src/TSTool_deletetiles.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_deletetiles.o: /C/prog/c++/tstool/src/TSTool_deletetiles.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_deletetiles.o /C/prog/c++/tstool/src/TSTool_deletetiles.cpp
 
-${OBJECTDIR}/_ext/1869484693/TSTool_extract.o: /C/prog/c++/tstool/src/TSTool_extract.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_extract.o /C/prog/c++/tstool/src/TSTool_extract.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_extract.o: /C/prog/c++/tstool/src/TSTool_extract.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_extract.o /C/prog/c++/tstool/src/TSTool_extract.cpp
 
-${OBJECTDIR}/_ext/1869484693/TSTool_info.o: /C/prog/c++/tstool/src/TSTool_info.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/TSTool_info.o /C/prog/c++/tstool/src/TSTool_info.cpp
+${OBJECTDIR}/_ext/6f6e1295/TSTool_info.o: /C/prog/c++/tstool/src/TSTool_info.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/TSTool_info.o /C/prog/c++/tstool/src/TSTool_info.cpp
 
-${OBJECTDIR}/_ext/1869484693/Tileset.o: /C/prog/c++/tstool/src/Tileset.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/Tileset.o /C/prog/c++/tstool/src/Tileset.cpp
+${OBJECTDIR}/_ext/6f6e1295/Tileset.o: /C/prog/c++/tstool/src/Tileset.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/Tileset.o /C/prog/c++/tstool/src/Tileset.cpp
 
-${OBJECTDIR}/_ext/1869484693/ToolManager.o: /C/prog/c++/tstool/src/ToolManager.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/ToolManager.o /C/prog/c++/tstool/src/ToolManager.cpp
+${OBJECTDIR}/_ext/6f6e1295/ToolManager.o: /C/prog/c++/tstool/src/ToolManager.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/ToolManager.o /C/prog/c++/tstool/src/ToolManager.cpp
 
-${OBJECTDIR}/_ext/1869484693/jsoncpp.o: /C/prog/c++/tstool/src/jsoncpp.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/jsoncpp.o /C/prog/c++/tstool/src/jsoncpp.cpp
+${OBJECTDIR}/_ext/6f6e1295/jsoncpp.o: /C/prog/c++/tstool/src/jsoncpp.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/jsoncpp.o /C/prog/c++/tstool/src/jsoncpp.cpp
 
-${OBJECTDIR}/_ext/1869484693/tstool.o: /C/prog/c++/tstool/src/tstool.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1869484693
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1869484693/tstool.o /C/prog/c++/tstool/src/tstool.cpp
+${OBJECTDIR}/_ext/6f6e1295/tstool.o: /C/prog/c++/tstool/src/tstool.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/6f6e1295
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/6f6e1295/tstool.o /C/prog/c++/tstool/src/tstool.cpp
 
-${OBJECTDIR}/src/TSTool_append.o: src/TSTool_append.cpp 
+${OBJECTDIR}/src/TSTool_append.o: src/TSTool_append.cpp
 	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TSTool_append.o src/TSTool_append.cpp
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/TSTool_append.o src/TSTool_append.cpp
 
-${OBJECTDIR}/src/TSTool_mapconverter.o: src/TSTool_mapconverter.cpp 
+${OBJECTDIR}/src/TSTool_mapconverter.o: src/TSTool_mapconverter.cpp
 	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TSTool_mapconverter.o src/TSTool_mapconverter.cpp
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/TSTool_mapconverter.o src/TSTool_mapconverter.cpp
 
-${OBJECTDIR}/src/TSTool_newtileset.o: src/TSTool_newtileset.cpp 
+${OBJECTDIR}/src/TSTool_newtileset.o: src/TSTool_newtileset.cpp
 	${MKDIR} -p ${OBJECTDIR}/src
-	${RM} $@.d
-	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/TSTool_newtileset.o src/TSTool_newtileset.cpp
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/C/MinGW/include/SDL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/src/TSTool_newtileset.o src/TSTool_newtileset.cpp
 
 # Subprojects
 .build-subprojects:
@@ -150,7 +150,6 @@ ${OBJECTDIR}/src/TSTool_newtileset.o: src/TSTool_newtileset.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} tstool-debug.exe
 
 # Subprojects
 .clean-subprojects:
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
+CND_PLATFORM_Debug=None-Windows
 CND_ARTIFACT_DIR_Debug=
 CND_ARTIFACT_NAME_Debug=tstool-debug
 CND_ARTIFACT_PATH_Debug=tstool-debug
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
 CND_PACKAGE_NAME_Debug=tstool.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/tstool.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/tstool.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW-Windows
+CND_PLATFORM_Release=None-Windows
 CND_ARTIFACT_DIR_Release=
 CND_ARTIFACT_NAME_Release=tstool
 CND_ARTIFACT_PATH_Release=tstool
-CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
 CND_PACKAGE_NAME_Release=tstool.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/tstool.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/tstool.tar
 #
 # include compiler specific variables
 #
